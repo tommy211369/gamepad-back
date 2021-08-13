170b12cb85eea2571dc34423efdb24afa2ba2c1e
tommy211369 <tommy.thongrasamy@gmail.com>
2021-08-13 19:40:42 +0200

    add user routes

--- routes/user.js
+++ routes/user.js
@@ -8,12 +8,24 @@ const isAuthenticated = require("../middlewares/isAuthenticated");
 // import User model
 const User = require("../models/User");
 
-// SIGN UP
+// SIGN UP (post)
+router.get("/signup", (req, res) => {
+  res.status(200).json({ message: "Sign Up route !" });
+});
 
-// LOGIN
+// LOGIN (post)
+router.get("/signin", (req, res) => {
+  res.status(200).json({ message: "Sign In route !" });
+});
 
-// FAVORITES
+// FAVORITES (post/get/delete)
+router.get("/user/favorites", (req, res) => {
+  res.status(200).json({ message: "Get user favorites !" });
+});
 
-// REVIEWS
+// REVIEWS (post/get/delete)
+router.get("/user/reviews", (req, res) => {
+  res.status(200).json({ message: "Get user reviews !" });
+});
 
 module.exports = router;
